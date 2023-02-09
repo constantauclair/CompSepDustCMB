0f09c3802f3d1a292c100fe7663998bed7ce2d21
constantauclair <constantauclair1998@gmail.com>
2023-02-09 16:01:16 +0100

    debug

--- test_biais_std/denoising_L1_bias_n_opt.py
+++ test_biais_std/denoising_L1_bias_n_opt.py
@@ -145,8 +145,8 @@ if __name__ == "__main__":
         coeffs_target = wph_op.apply(torch.from_numpy(Mixture), norm=norm, pbc=pbc) - bias # estimation of the unbiased coefficients
         
         # Minimization
-        #result = opt.minimize(objective, Noise_tilde.cpu().ravel(), method='L-BFGS-B', jac=True, tol=None, options=optim_params)
-        result = opt.minimize(objective, np.zeros(np.shape(Mixture)).ravel(), method='L-BFGS-B', jac=True, tol=None, options=optim_params)
+        result = opt.minimize(objective, Noise_tilde.cpu().ravel(), method='L-BFGS-B', jac=True, tol=None, options=optim_params)
+        #result = opt.minimize(objective, np.zeros(np.shape(Mixture)).ravel(), method='L-BFGS-B', jac=True, tol=None, options=optim_params)
         final_loss, Noise_tilde, niter, msg = result['fun'], result['x'], result['nit'], result['message']
         
         # Reshaping
