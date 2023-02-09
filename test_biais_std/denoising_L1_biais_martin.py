96dbe22b7592668ba274f2fa4e5143f37f0ca6b9
constantauclair <constantauclair1998@gmail.com>
2023-02-09 10:47:46 +0100

    debug

--- test_biais_std/denoising_L1_biais_martin.py
+++ test_biais_std/denoising_L1_biais_martin.py
@@ -24,9 +24,9 @@ norm="auto"
 SNR = 1
 
 n_step = 10
-iter_per_step = 20
+iter_per_step = 50
 
-#optim_params = {"maxiter": iter_per_step, "gtol": 1e-14, "ftol": 1e-14, "maxcor": 20}
+optim_params = {"maxiter": iter_per_step, "gtol": 1e-14, "ftol": 1e-14, "maxcor": 20}
 
 device = 0 # GPU to use
 
@@ -147,7 +147,7 @@ if __name__ == "__main__":
         
         # Minimization
         #result = opt.minimize(objective, Dust_tilde.cpu().ravel(), method='L-BFGS-B', jac=True, tol=None, options=optim_params)
-        result = opt.minimize(objective, torch.from_numpy(Mixture).ravel(), method='L-BFGS-B', jac=True, tol=None, options={"maxiter": iter_per_step*(i+1), "gtol": 1e-14, "ftol": 1e-14, "maxcor": 20})
+        result = opt.minimize(objective, torch.from_numpy(Mixture).ravel(), method='L-BFGS-B', jac=True, tol=None, options=optim_params)
         final_loss, Dust_tilde, niter, msg = result['fun'], result['x'], result['nit'], result['message']
         
         # Reshaping
