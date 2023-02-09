8e18e29eb525d7be7a1c93de9c8aad821a550357
constantauclair <constantauclair1998@gmail.com>
2023-02-09 16:04:34 +0100

    debug

--- test_biais_std/denoising_L1_bias_n_opt.py
+++ test_biais_std/denoising_L1_bias_n_opt.py
@@ -24,7 +24,7 @@ norm="auto"
 SNR = 1
 
 n_step = 10
-iter_per_step = 20
+iter_per_step = 30
 
 optim_params = {"maxiter": iter_per_step, "gtol": 1e-14, "ftol": 1e-14, "maxcor": 20}
 
@@ -145,8 +145,8 @@ if __name__ == "__main__":
         coeffs_target = wph_op.apply(torch.from_numpy(Mixture), norm=norm, pbc=pbc) - bias # estimation of the unbiased coefficients
         
         # Minimization
-        result = opt.minimize(objective, Noise_tilde.cpu().ravel(), method='L-BFGS-B', jac=True, tol=None, options=optim_params)
-        #result = opt.minimize(objective, np.zeros(np.shape(Mixture)).ravel(), method='L-BFGS-B', jac=True, tol=None, options=optim_params)
+        #result = opt.minimize(objective, Noise_tilde.cpu().ravel(), method='L-BFGS-B', jac=True, tol=None, options=optim_params)
+        result = opt.minimize(objective, np.zeros(np.shape(Mixture)).ravel(), method='L-BFGS-B', jac=True, tol=None, options=optim_params)
         final_loss, Noise_tilde, niter, msg = result['fun'], result['x'], result['nit'], result['message']
         
         # Reshaping
