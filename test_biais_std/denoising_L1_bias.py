921611a2ddb694b164376cf1f02b230662d58648
constantauclair <constantauclair1998@gmail.com>
2023-02-09 11:45:07 +0100

    debug

--- test_biais_std/denoising_L1_bias.py
+++ test_biais_std/denoising_L1_bias.py
@@ -21,9 +21,9 @@ dn = 2
 pbc = True
 norm="auto"
 
-SNR = 0.5
+SNR = 1
 
-n_step = 10
+n_step = 20
 iter_per_step = 20
 
 optim_params = {"maxiter": iter_per_step, "gtol": 1e-14, "ftol": 1e-14, "maxcor": 20}
