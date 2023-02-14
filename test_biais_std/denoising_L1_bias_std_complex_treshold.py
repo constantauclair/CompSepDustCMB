7c3ab116512edbd6dfb7d8471b1a6cc6d306925c
constantauclair <constantauclair1998@gmail.com>
2023-02-14 12:04:40 +0100

    debug

--- test_biais_std/denoising_L1_bias_std_complex_treshold.py
+++ test_biais_std/denoising_L1_bias_std_complex_treshold.py
@@ -225,7 +225,7 @@ if __name__ == "__main__":
     
     eval_cnt = 0
     
-    wph_op.load_model(["S11","S00","S01","Cphase","C01","C00","L"])
+    wph_op.load_model(["S11","S00","S01","Cphase","C01","C00"])
     
     wph_op.clear_normalization()
     coeffs_imag = torch.imag(wph_op.apply(Dust_tilde0,norm='auto',pbc=pbc))
