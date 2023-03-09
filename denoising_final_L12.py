47df1361d7f2434504774fa3c83e1372e25b78c2
constantauclair <constantauclair1998@gmail.com>
2023-03-09 14:44:30 +0100

    debug

--- denoising_final_L12.py
+++ denoising_final_L12.py
@@ -22,10 +22,10 @@ SNR = 1
 
 file_name="denoising_final_L12_SNR="+str(SNR)+".npy"
 
-n_step1 = 1
+n_step1 = 5#1
 iter_per_step1 = 50
 
-n_step2 = 10
+n_step2 = 5#10
 iter_per_step2 = 50
 
 optim_params1 = {"maxiter": iter_per_step1, "gtol": 1e-14, "ftol": 1e-14, "maxcor": 20}
