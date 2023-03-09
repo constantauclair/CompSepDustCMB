58b03807348a556a1a456f8b84e5ffe345719508
constantauclair <constantauclair1998@gmail.com>
2023-03-09 08:56:45 +0100

    debug

--- denoising_final_L12.py
+++ denoising_final_L12.py
@@ -303,7 +303,7 @@ if __name__ == "__main__":
         # Minimization
         #result = opt.minimize(objective2, Dust_tilde.cpu().ravel(), method='L-BFGS-B', jac=True, tol=None, options=optim_params2)
         #result = opt.minimize(objective2, torch.from_numpy(Mixture).ravel(), method='L-BFGS-B', jac=True, tol=None, options=optim_params2)
-        result = opt.minimize(objective2, torch.from_numpy(Mixture-Noise*1.2).ravel(), method='L-BFGS-B', jac=True, tol=None, options=optim_params2)
+        result = opt.minimize(objective2, torch.from_numpy(Mixture-Noise).ravel(), method='L-BFGS-B', jac=True, tol=None, options=optim_params2)
         final_loss, Dust_tilde, niter, msg = result['fun'], result['x'], result['nit'], result['message']
         
         # Reshaping
