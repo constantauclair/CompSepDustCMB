e8a445a42ca6cbbd8dae9d3eb96be539aa8f993c
constantauclair <constantauclair1998@gmail.com>
2023-03-10 14:28:47 +0100

    debug

--- denoising_multifreq_L12.py
+++ denoising_multifreq_L12.py
@@ -28,7 +28,7 @@ n_step1 = 5
 iter_per_step1 = 50
 
 n_step2 = 10
-iter_per_step2 = 50
+iter_per_step2 = 100
 
 optim_params1 = {"maxiter": iter_per_step1, "gtol": 1e-14, "ftol": 1e-14, "maxcor": 20}
 optim_params2 = {"maxiter": iter_per_step2, "gtol": 1e-14, "ftol": 1e-14, "maxcor": 20}
