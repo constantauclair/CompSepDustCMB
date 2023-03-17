1cb79ca48ff894f15cb72a7a7d8ffc656de8e98a
constantauclair <constantauclair1998@gmail.com>
2023-03-17 14:39:10 +0100

    debug

--- separation_multifreq_L123_oldstyle.py
+++ separation_multifreq_L123_oldstyle.py
@@ -429,7 +429,7 @@ if __name__ == "__main__":
         
         # Coeffs target computation
         # L1
-        coeffs_target_L1 = torch.cat((torch.unsqueeze(torch.real(wph_op.apply(torch.from_numpy(Mixture), norm=None, pbc=pbc)),dim=0),torch.unsqueeze(torch.imag(wph_op.apply(torch.from_numpy(Mixture), norm=None, pbc=pbc),dim=0))))
+        coeffs_target_L1 = torch.cat((torch.unsqueeze(torch.real(wph_op.apply(torch.from_numpy(Mixture), norm=None, pbc=pbc)),dim=0),torch.unsqueeze(torch.imag(wph_op.apply(torch.from_numpy(Mixture), norm=None, pbc=pbc)),dim=0)))
         # L2
         coeffs_target_L2 = mean_L2
         # L3
