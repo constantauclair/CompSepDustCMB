96186fca026f1efe5f4bf48b9ddcd30adfa1935b
constantauclair <constantauclair1998@gmail.com>
2023-03-21 16:38:17 +0100

    debug

--- separation_multifreq_L1234.py
+++ separation_multifreq_L1234.py
@@ -37,8 +37,8 @@ file_name="separation_multifreq_L1234_0,5.npy"
 n_step1 = 5
 iter_per_step1 = 50
 
-n_step2 = 10
-iter_per_step2 = 100
+n_step2 = 5#10
+iter_per_step2 = 200#100
 
 optim_params1 = {"maxiter": iter_per_step1, "gtol": 1e-14, "ftol": 1e-14, "maxcor": 20}
 optim_params2 = {"maxiter": iter_per_step2, "gtol": 1e-14, "ftol": 1e-14, "maxcor": 20}
