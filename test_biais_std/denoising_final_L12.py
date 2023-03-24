181bc64c91741348d1f62aecc23c6904ee3ba864
constantauclair <constantauclair1998@gmail.com>
2023-03-24 13:50:55 +0100

    debug

--- test_biais_std/denoising_final_L12.py
+++ test_biais_std/denoising_final_L12.py
@@ -25,8 +25,8 @@ file_name="denoising_final_L12_SNR=0,25.npy"
 n_step1 = 5
 iter_per_step1 = 50
 
-n_step2 = 10
-iter_per_step2 = 50
+n_step2 = 5#10
+iter_per_step2 = 100#50
 
 optim_params1 = {"maxiter": iter_per_step1, "gtol": 1e-14, "ftol": 1e-14, "maxcor": 20}
 optim_params2 = {"maxiter": iter_per_step2, "gtol": 1e-14, "ftol": 1e-14, "maxcor": 20}
