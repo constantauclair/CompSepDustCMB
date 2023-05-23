f7f709204e2c303dfec57e2269d887b46291a812
constantauclair <constantauclair1998@gmail.com>
2023-05-23 11:33:28 +0200

    debug

--- generate_flat_noise_Planck_maps.py
+++ generate_flat_noise_Planck_maps.py
@@ -33,11 +33,11 @@ import torch
 IQU_Noise_maps = torch.zeros((3,n_noise_maps,N,N))
 for i in range(n_noise_maps):
     noise_map = fits.open(noise_folder+'product-action?SIMULATED_MAP.FILE_ID=ffp10_noise_'+str(freq)+'_full_map_mc_000'+str(1000+i)[-2:]+'.fits')
-    IQU_Noise_maps[0,i] = torch.tensor(hp.gnomview((hp.read_map(noise_map,field=0)*1e6).to(0),coord='G',rot=[longitude,latitude],reso=reso,xsize=N,ysize=N,return_projected_map=True,cmap='inferno')).cpu()
+    IQU_Noise_maps[0,i] = torch.tensor(hp.gnomview(torch.from_numpy(hp.read_map(noise_map,field=0)*1e6).to(0),coord='G',rot=[longitude,latitude],reso=reso,xsize=N,ysize=N,return_projected_map=True,cmap='inferno')).cpu()
     plt.close()
-    IQU_Noise_maps[1,i] = torch.tensor(hp.gnomview((hp.read_map(noise_map,field=1)*1e6).to(0),coord='G',rot=[longitude,latitude],reso=reso,xsize=N,ysize=N,return_projected_map=True,cmap='inferno')).cpu()
+    IQU_Noise_maps[1,i] = torch.tensor(hp.gnomview(torch.from_numpy(hp.read_map(noise_map,field=1)*1e6).to(0),coord='G',rot=[longitude,latitude],reso=reso,xsize=N,ysize=N,return_projected_map=True,cmap='inferno')).cpu()
     plt.close()
-    IQU_Noise_maps[2,i] = torch.tensor(hp.gnomview((hp.read_map(noise_map,field=2)*1e6).to(0),coord='G',rot=[longitude,latitude],reso=reso,xsize=N,ysize=N,return_projected_map=True,cmap='inferno')).cpu()
+    IQU_Noise_maps[2,i] = torch.tensor(hp.gnomview(torch.from_numpy(hp.read_map(noise_map,field=2)*1e6).to(0),coord='G',rot=[longitude,latitude],reso=reso,xsize=N,ysize=N,return_projected_map=True,cmap='inferno')).cpu()
     plt.close()
     print(i)
 
