383feb51dd77057d40e8839fac535c6721b261e4
constantauclair <constantauclair1998@gmail.com>
2023-05-24 15:58:54 +0200

    debug

--- separation_multifreq_L12345678_Q_simple.py
+++ separation_multifreq_L12345678_Q_simple.py
@@ -132,9 +132,7 @@ def compute_coeffs_mean_std(mode,contamination_batch,cross_contamination_batch=N
         computed_conta = 0
         for i in range(n_batch):
             batch_COEFFS = torch.zeros((n_freq,batch_size,coeffs_number)).type(dtype=ref_type)
-            print(x.cpu().size())
-            print(contamination_batch[:,i].cpu().size())
-            u_noisy, nb_chunks = wph_op.preconfigure(x + contamination_batch[:,i], pbc=pbc)
+            u_noisy, nb_chunks = wph_op.preconfigure(x.expand(contamination_batch[:,i].size()) + contamination_batch[:,i], pbc=pbc)
             for j in range(nb_chunks):
                 coeffs_chunk, indices = wph_op.apply(u_noisy, j, norm=None, ret_indices=True, pbc=pbc)
                 batch_COEFFS[:,:,indices] = coeffs_chunk - wph_op.apply(x, norm=None, pbc=pbc)[:,indices]
