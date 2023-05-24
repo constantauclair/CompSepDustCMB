c81264b3d088faca96d2f55fb70c76430aeb57bc
constantauclair <constantauclair1998@gmail.com>
2023-05-24 16:39:06 +0200

    debug

--- separation_multifreq_L12345678_Q_simple.py
+++ separation_multifreq_L12345678_Q_simple.py
@@ -213,10 +213,10 @@ def compute_coeffs_mean_std(mode,contamination_batch,cross_contamination_batch=N
 def compute_mask(x,std,real_imag=True,cross=False):
     coeffs = wph_op.apply(x,norm=None,pbc=pbc,cross=cross)
     if not real_imag:
-        mask = torch.logical_and(torch.abs(coeffs) > 1e-7, torch.abs(std) > 0)
+        mask = torch.logical_and(torch.abs(coeffs).to(device) > 1e-7, torch.abs(std).to(device) > 0)
     if real_imag:
-        mask_real = torch.logical_and(torch.real(coeffs) > 1e-7, std[0] > 0)
-        mask_imag = torch.logical_and(torch.imag(coeffs) > 1e-7, std[1] > 0)
+        mask_real = torch.logical_and(torch.real(coeffs).to(device) > 1e-7, std[0].to(device) > 0)
+        mask_imag = torch.logical_and(torch.imag(coeffs).to(device) > 1e-7, std[1].to(device) > 0)
         mask = torch.cat((torch.unsqueeze(mask_real,dim=0),torch.unsqueeze(mask_imag,dim=0)))
     return mask
 
