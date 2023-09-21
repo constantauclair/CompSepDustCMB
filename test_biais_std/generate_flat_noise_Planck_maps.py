1e0acc1416e3beb1261b5797339f020f51036874
constantauclair <constantauclair1998@gmail.com>
2023-09-21 17:41:46 +0200

    debug

--- test_biais_std/generate_flat_noise_Planck_maps.py
+++ test_biais_std/generate_flat_noise_Planck_maps.py
@@ -13,12 +13,13 @@ N = 512
 
 freq = 143
 
+n_start = 10
 n_noise_maps = 100
 noise_folder = '../data/IQU_Planck_data/Planck_noise_fits/'
 
 IQU_Noise_maps = np.zeros((3,n_noise_maps,N,N))
 for i in range(n_noise_maps):
-    noise_map = fits.open(noise_folder+'product-action?SIMULATED_MAP.FILE_ID=ffp10_noise_'+str(freq)+'_full_map_mc_000'+str(1000+i)[-2:]+'.fits')
+    noise_map = fits.open(noise_folder+'product-action?SIMULATED_MAP.FILE_ID=ffp10_noise_'+str(freq)+'_full_map_mc_000'+str(1000+n_start+i)[-2:]+'.fits')
     IQU_Noise_maps[0,i] = np.array(hp.gnomview(hp.read_map(noise_map,field=0)*1e6,coord='G',rot=[longitude,latitude],reso=reso,xsize=N,ysize=N,return_projected_map=True,cmap='inferno'))
     plt.close()
     IQU_Noise_maps[1,i] = np.array(hp.gnomview(hp.read_map(noise_map,field=1)*1e6,coord='G',rot=[longitude,latitude],reso=reso,xsize=N,ysize=N,return_projected_map=True,cmap='inferno'))
