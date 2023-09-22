0051886e2cb51f09a62b6271bf1a3f9bb20426fc
constantauclair <constantauclair1998@gmail.com>
2023-09-22 09:02:17 +0200

    debug

--- test_biais_std/generate_flat_noise_Planck_maps.py
+++ test_biais_std/generate_flat_noise_Planck_maps.py
@@ -13,20 +13,18 @@ N = 512
 
 freq = 143
 
-forbid_noises = [18]
-n_noise_maps = 100 - len(forbid_noises)
+n_noise_maps = 1
 noise_folder = '../data/IQU_Planck_data/Planck_noise_fits/'
 
 IQU_Noise_maps = np.zeros((3,n_noise_maps,N,N))
 for i in range(n_noise_maps):
-    if i not in forbid_noises:
-        noise_map = fits.open(noise_folder+'product-action?SIMULATED_MAP.FILE_ID=ffp10_noise_'+str(freq)+'_full_map_mc_000'+str(1000+i)[-2:]+'.fits')
-        IQU_Noise_maps[0,i] = np.array(hp.gnomview(hp.read_map(noise_map,field=0)*1e6,coord='G',rot=[longitude,latitude],reso=reso,xsize=N,ysize=N,return_projected_map=True,cmap='inferno'))
-        plt.close()
-        IQU_Noise_maps[1,i] = np.array(hp.gnomview(hp.read_map(noise_map,field=1)*1e6,coord='G',rot=[longitude,latitude],reso=reso,xsize=N,ysize=N,return_projected_map=True,cmap='inferno'))
-        plt.close()
-        IQU_Noise_maps[2,i] = np.array(hp.gnomview(hp.read_map(noise_map,field=2)*1e6,coord='G',rot=[longitude,latitude],reso=reso,xsize=N,ysize=N,return_projected_map=True,cmap='inferno'))
-        plt.close()
-        print(i)
+    noise_map = fits.open(noise_folder+'product-action?SIMULATED_MAP.FILE_ID=ffp10_noise_'+str(freq)+'_full_map_mc_000'+str(1000+100+i)[-2:]+'.fits')
+    IQU_Noise_maps[0,i] = np.array(hp.gnomview(hp.read_map(noise_map,field=0)*1e6,coord='G',rot=[longitude,latitude],reso=reso,xsize=N,ysize=N,return_projected_map=True,cmap='inferno'))
+    plt.close()
+    IQU_Noise_maps[1,i] = np.array(hp.gnomview(hp.read_map(noise_map,field=1)*1e6,coord='G',rot=[longitude,latitude],reso=reso,xsize=N,ysize=N,return_projected_map=True,cmap='inferno'))
+    plt.close()
+    IQU_Noise_maps[2,i] = np.array(hp.gnomview(hp.read_map(noise_map,field=2)*1e6,coord='G',rot=[longitude,latitude],reso=reso,xsize=N,ysize=N,return_projected_map=True,cmap='inferno'))
+    plt.close()
+    print(i)
 
 np.save("../data/IQU_Planck_data/Chameleon-Musca data/Noise_IQU_"+str(freq)+".npy",IQU_Noise_maps)
