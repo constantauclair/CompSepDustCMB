3a90675b775d517a916ee32b996d3b7a954c2a7f
constantauclair <constantauclair1998@gmail.com>
2023-09-22 15:49:21 +0200

    debug

--- test_biais_std/generate_TEB_flat_noise_Planck_maps.py
+++ test_biais_std/generate_TEB_flat_noise_Planck_maps.py
@@ -21,7 +21,9 @@ TEB_Noise_maps = np.zeros((3,n_noise_maps,N,N))
 for i in range(n_noise_maps):
     noise_map = fits.open(noise_folder+'product-action?SIMULATED_MAP.FILE_ID=ffp10_noise_'+str(freq)+'_full_map_mc_000'+str(1000+i)[-2:]+'.fits')
     IQU_map = [hp.read_map(noise_map,field=0),hp.read_map(noise_map,field=1),hp.read_map(noise_map,field=2)]
+    print("Map loaded !")
     TEB_alm = hp.map2alm(IQU_map)
+    print("Alm computed !")
     T_map = hp.alm2map(TEB_alm[0],nside)
     E_map = hp.alm2map(TEB_alm[1],nside)
     B_map = hp.alm2map(TEB_alm[2],nside)
@@ -31,6 +33,6 @@ for i in range(n_noise_maps):
     plt.close()
     TEB_Noise_maps[2,i] = np.array(hp.gnomview(B_map,coord='G',rot=[longitude,latitude],reso=reso,xsize=N,ysize=N,return_projected_map=True,cmap='inferno'))
     plt.close()
-    print(i)
+    print(i,"done !")
 
 np.save("../data/IQU_Planck_data/TE correlation data/Noise_TEB_"+str(freq)+".npy",TEB_Noise_maps)
