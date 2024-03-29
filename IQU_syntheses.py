fe5d4de5c4101a0e43ed588ee73a65ec319dddd8
constantauclair <constantauclair1998@gmail.com>
2024-03-29 15:54:31 +0100

    debug

--- IQU_syntheses.py
+++ IQU_syntheses.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 sys.path.append('../Packages/dust_genmodels-main/code/')
 from synth import get_initialization, synthesis
+from astropy.convolution import convolve, Gaussian2DKernel
 
 separation = np.load('separation_IQU_B_353_5steps_50iters_Mn=100.npy')[:,64:-64,64:-64].astype(np.float64)
 
@@ -13,21 +14,23 @@ optim_params = {"maxiter": 100}
 nsynth = 1
 cross_pairs = [[0,1],[0,2],[1,2]]
 
-I = separation[0]
-Q = separation[2]
-U = separation[6]
+sigma = 1
+gauss = Gaussian2DKernel(sigma)
+
+I = convolve(np.log(separation[0]),gauss)
+Q = convolve(separation[2],gauss)
+U = convolve(separation[6],gauss)
 
 x_IQU = np.array([I,Q,U])
-#x_IQU[0] = np.log(x_IQU[0])
 x_std = x_IQU.std(axis=(-1, -2),keepdims=True)
 x_mean = x_IQU.mean(axis=(-1, -2), keepdims=True)
-x_IQU = (x_IQU - x_mean) / x_std
+x_target = (x_IQU - x_mean) / x_std
 
-x_0 = get_initialization(x_IQU, nsynth=nsynth)
-x_s_ = synthesis(x_IQU, x_0, J, L, dn, device=device, optim_params=optim_params, cross_pairs=cross_pairs, wph_model=['S11','S00','S01','Cphase','C00','C01'])
+x_0 = get_initialization(x_target, nsynth=nsynth)
+x_s = synthesis(x_target, x_0, J, L, dn, device=device, optim_params=optim_params, cross_pairs=cross_pairs, wph_model=['S11','S00','S01','Cphase','C00','C01'])
 
-x_s = x_s_ * x_std + x_mean
-#x_s[:,0] = np.exp(x_s[:,0])
-x_f = x_s    
+x_syn = x_s * x_std + x_mean
 
-np.save('IQU_synthesis.npy',x_f)
+x_final = np.concatenate((np.array([x_IQU]),x_syn),axis=0)  
+
+np.save('IQU_synthesis.npy',x_final)
