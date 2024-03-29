bdadc447a0106a3cb7b9ac5050324a35449c2882
constantauclair <constantauclair1998@gmail.com>
2024-03-29 14:44:29 +0100

    debug

--- IQU_syntheses.py
+++ IQU_syntheses.py
@@ -18,7 +18,7 @@ Q = separation[2]
 U = separation[6]
 
 x_IQU = np.array([I,Q,U])
-#x_IQU[0] = np.log(x_IQU[0])
+x_IQU[0] = np.log(x_IQU[0])
 x_std = x_IQU.std(axis=(-1, -2),keepdims=True)
 x_mean = x_IQU.mean(axis=(-1, -2), keepdims=True)
 x_IQU = (x_IQU - x_mean) / x_std
@@ -27,7 +27,7 @@ x_0 = get_initialization(x_IQU, nsynth=nsynth)
 x_s_ = synthesis(x_IQU, x_0, J, L, dn, device=device, optim_params=optim_params, cross_pairs=cross_pairs)
 
 x_s = x_s_ * x_std + x_mean
-#x_s[:,0] = np.exp(x_s[:,0])
+x_s[:,0] = np.exp(x_s[:,0])
 x_f = x_s    
 
 np.save('IQU_synthesis.npy',x_f)
