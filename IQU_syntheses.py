e24fa4ece7dcf61b1dfdf3b9c8f058c40936322d
constantauclair <constantauclair1998@gmail.com>
2024-04-02 15:42:38 +0200

    debug

--- IQU_syntheses.py
+++ IQU_syntheses.py
@@ -2,9 +2,10 @@ import sys
 import numpy as np
 sys.path.append('../Packages/dust_genmodels-main/code/')
 from synth import get_initialization, synthesis
-from astropy.convolution import convolve, Gaussian2DKernel
+#from astropy.convolution import convolve, Gaussian2DKernel
 
 separation = np.load('separation_IQU_B_353_5steps_50iters_Mn=100.npy')[:,64:-64,64:-64].astype(np.float64)
+I = np.load('data/IQU_Planck_data/Sroll20_data/Sroll20_IQU_maps.npy')[0,3,0,64:-64,64:-64].astype(np.float32)
 
 J = 7
 L = 4
@@ -21,7 +22,7 @@ cross_pairs = [[0,1],[0,2],[1,2]]
 # Q = convolve(separation[2],gauss)
 # U = convolve(separation[6],gauss)
 
-I = np.log(separation[0])
+I = np.log(I)
 Q = separation[2]
 U = separation[6]
 
